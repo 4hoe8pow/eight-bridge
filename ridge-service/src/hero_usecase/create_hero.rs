046040c3ab7fb3ee39c3a9fa7c31e7addb4874fe
4hoe8pow <124800114+4hoe8pow@users.noreply.github.com>
2024-01-23 15:48:03 +0900

    Update create_hero.rs

--- ridge-service/src/hero_usecase/create_hero.rs
+++ ridge-service/src/hero_usecase/create_hero.rs
@@ -1,9 +1,20 @@
 use bevy::prelude::*;
-use ridge_domain::ridge_hero::hero::HeroPosition;
+use ridge_domain::ridge_hero::hero::Hero;
+
+#[derive(Resource, Default, Debug)]
+pub struct HeroPositions {
+    pub hero: Vec<Hero>,
+}
+
+impl HeroPositions {
+    pub fn add_hero(&mut self, new_hero: Hero) {
+        self.hero.push(new_hero);
+    }
+}
 
 /// ヒーローポジショニング
-pub fn set_hero(mut commands: Commands) {
-    commands.insert_resource(HeroPosition {
+pub fn init_hero(mut hero_positions: ResMut<HeroPositions>) {
+    hero_positions.add_hero(Hero {
         row: 0,
         col: 6,
         past_row: -1,
